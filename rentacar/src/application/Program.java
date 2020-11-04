ab517f3c771bf5e6387fc7f503c654bffc9655dc
Cristiano Nascimento <cs_nascimento@yahoo.com.br>
2020-11-04 00:54:34 -0300

    Parte 2 Solução sem Interface

--- rentacar/src/application/Program.java
+++ rentacar/src/application/Program.java
@@ -1,7 +1,54 @@
 package application;
 
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.CarRental;
+import model.entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
+
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException{
         
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:ss");
+
+        System.out.println("Enter rental data: ");
+        
+        System.out.print("Car model: ");
+        String carModel = sc.nextLine();
+
+        System.out.print("Pickup (dd/MM/yyyy hh:mm): ");
+        Date start = sdf.parse(sc.nextLine());
+
+        System.out.print("Return (dd/MM/yyyy hh:mm): ");
+        Date finish = sdf.parse(sc.nextLine());
+
+        CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+        System.out.print("Enter price per hour: ");
+        Double pricePerHour = sc.nextDouble();
+
+        System.out.print("Enter price per day: ");
+        Double pricePerDay = sc.nextDouble();
+
+        RentalService rentalService = new RentalService(pricePerDay, pricePerHour, new BrazilTaxService());
+
+        rentalService.processInvoice(cr);
+
+        System.out.println("INVOICE:");
+        System.out.printf("Basic payment: %.2f", cr.getInvoice().getBasicPayment());
+        System.out.println();
+        System.out.printf("Tax: %.2f", cr.getInvoice().getTax());
+        System.out.println();
+        System.out.printf("Total payment: %.2f", cr.getInvoice().getTotalPayment());
+        
+        sc.close();
+
     }
 }
